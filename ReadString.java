74f7afb8ba350e3d0b64eda9995236530cf09475
kiennguyenGH <ktnguyen4647@gmail.com>
2022-04-12 10:42:38 -0700

    Add states

--- ReadString.java
+++ ReadString.java
@@ -1,10 +1,20 @@
 public class ReadString {
+
+    private enum states {
+        start,
+        num,
+        numWDecimal,
+        fail
+    }
+
+
     public float GetFloat(String input)
     {
-        String number = "1000";
+        String number = "";
         float finalNumber;
         for (int i = 0; i < input.length(); i++)
         {
+            
 
             if (input.charAt(i) == '9')
             {
