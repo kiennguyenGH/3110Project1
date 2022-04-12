56512070924b7650ee6f1524f7d06e2dc9b03a3e
kiennguyenGH <ktnguyen4647@gmail.com>
2022-04-12 10:10:47 -0700

    Adjust return value

--- ReadString.java
+++ ReadString.java
@@ -1,13 +1,15 @@
 public class ReadString {
     public float GetFloat(String input)
     {
+        String number = "";
         for (int i = 0; i < input.length(); i++)
         {
             if (input.charAt(i) == '9')
             {
-                
+                continue;
             }
         }
-        return 0; 
+        float finalNumber = Float.parseFloat(number);
+        return finalNumber; 
     }
 }
